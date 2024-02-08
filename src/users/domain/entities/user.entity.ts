518b170e2eebbe4fba262e0193b95f844e7959f5
fersilverio <ssilveriofernando45@gmail.com>
2024-02-07 22:11:04 -0300

    - Integration tests for class validator - Validation structure for user entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity } from "@/shared/domain/entities/entity"
+import { UserValidatorFactory } from "../validators/user.validator"
 
 export type UserProps = {
   name: string
@@ -11,16 +12,19 @@ export type UserProps = {
 
 export class UserEntity extends Entity<UserProps> {
   constructor(public readonly props: UserProps, id?: string) {
+    UserEntity.validate(props)
     super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
 
   update(value: string): void {
+    UserEntity.validate({ ...this.props, name: value })
     this.name = value
   }
 
   updatePassword(value: string): void {
+    UserEntity.validate({ ...this.props, password: value })
     this.password = value
   }
 
@@ -47,5 +51,11 @@ export class UserEntity extends Entity<UserProps> {
 
   get createdAt() {
     return this.props.createdAt
+  }
+
+
+  static validate(props: UserProps) {
+    const validator = UserValidatorFactory.create()
+    validator.validate(props)
   }
 }
