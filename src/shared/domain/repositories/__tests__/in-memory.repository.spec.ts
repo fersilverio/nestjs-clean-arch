2f758f993c8747ce7d4f2e5d83e2dbef9ba03327
fersilverio <ssilveriofernando45@gmail.com>
2024-03-04 20:40:20 -0300

    InMemoryRepository unit tests - insert method

---
+++ src/shared/domain/repositories/__tests__/in-memory.repository.spec.ts
@@ -0,0 +1,30 @@
+import { Entity } from "../../entities/entity"
+import { InMemoryRepository } from "../in-memory.repository"
+
+type StubEntityProps = {
+  name: string
+  price: number
+}
+
+class StubEntity extends Entity<StubEntityProps> { }
+
+
+class StubInMemoryRepository extends InMemoryRepository<StubEntity> {
+
+}
+
+describe("InMemoryRepository unit tests", () => {
+  let sut = new StubInMemoryRepository()
+
+  beforeEach(() => {
+    sut = new StubInMemoryRepository()
+  })
+
+  it("Should insert a new entity", async () => {
+    const entity = new StubEntity({ name: "test", price: 10 })
+
+    await sut.insert(entity)
+
+    expect(entity.toJSON()).toStrictEqual(sut.items[0].toJSON())
+  })
+})
