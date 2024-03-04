a168e79133d4d50702e2a44bae73d6a635610952
fersilverio <ssilveriofernando45@gmail.com>
2024-03-04 20:52:15 -0300

    InMemoryRepository unit tests - getAll method

--- src/shared/domain/repositories/__tests__/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/in-memory.repository.spec.ts
@@ -41,4 +41,13 @@ describe("InMemoryRepository unit tests", () => {
 
     expect(entity.toJSON()).toStrictEqual(result.toJSON())
   })
+
+  it("Should return all entities", async () => {
+    const entity = new StubEntity({ name: "test", price: 10 })
+
+    await sut.insert(entity)
+    const result = await sut.findAll()
+
+    expect([entity]).toStrictEqual(result)
+  })
 })
