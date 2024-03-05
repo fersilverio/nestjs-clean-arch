2a7e0746b2b2e0bd435b6d3ce7ecf6b11381b202
fersilverio <ssilveriofernando45@gmail.com>
2024-03-04 21:44:22 -0300

    Adding SearchableRepositoryInterface

---
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -0,0 +1,6 @@
+import { Entity } from "../entities/entity";
+import { RepositoryInterface } from "./repository-contracts";
+
+export interface SearchableRepositoryInterface<E extends Entity, SearchInput, SearchOutput> extends RepositoryInterface<E> {
+  search(props: SearchInput): Promise<SearchOutput>
+}
--- src/users/infrastructure/databse/in-memory/user-in-memory.repository.ts
+++ src/users/infrastructure/databse/in-memory/user-in-memory.repository.ts
@@ -1,10 +1,10 @@
 import { ConflictError } from "@/shared/domain/errors/conflict-error";
 import { NotFoundError } from "@/shared/domain/errors/not-found-error";
-import { InMemoryRepository } from "@/shared/domain/repositories/in-memory.repository";
+import { InMemorySearchableRepository } from "@/shared/domain/repositories/in-memory-searchable.repository";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { UserRepository } from "@/users/domain/repositories/user.repository";
 
-export class UserInMemoryRepository extends InMemoryRepository<UserEntity> implements UserRepository {
+export class UserInMemoryRepository extends InMemorySearchableRepository<UserEntity> implements UserRepository {
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find(item => item.email === email);
 
