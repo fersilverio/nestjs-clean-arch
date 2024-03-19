0e8585388e6df5eaf0aaed855afd7fa0f1c48b13
fersilverio <ssilveriofernando45@gmail.com>
2024-03-19 20:55:05 -0300

    Adding ListUsersUsecase - part 1

--- src/users/application/usecases/__tests__/unit/signup.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/signup.usecase.spec.ts
@@ -1,6 +1,6 @@
 import { HashProvider } from "@/shared/application/providers/hash-provider"
 import { ConflictError } from "@/shared/domain/errors/conflict-error"
-import { BadRequestError } from "@/users/application/errors/bad-request-error"
+import { BadRequestError } from "@/shared/application/errors/bad-request-error"
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
 import { UserInMemoryRepository } from "@/users/infrastructure/database/in-memory/user-in-memory.repository"
 import { BcryptjsHashProvider } from "@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider"
---
+++ src/users/application/usecases/listusers.usecase.ts
@@ -0,0 +1,24 @@
+import { UserRepository } from "@/users/domain/repositories/user.repository"
+import { UserOutput } from "../dtos/user-output.dto";
+import { UseCase as DefaultUseCase } from "@/shared/application/usecases/use-case"
+import { SearchInput } from "@/shared/application/dtos/search-input.dto";
+
+
+export namespace ListUsersUseCase {
+  export type Input = SearchInput
+
+  export type Output = void
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(
+      private userRepository: UserRepository.Repository,
+    ) { }
+
+    async execute(input: Input): Promise<Output> {
+      const params = new UserRepository.SearchParams(input)
+      const searchResult = await this.userRepository.search(params);
+
+      return
+    }
+  }
+}
