171a21b39a6df969b89da192d827f73e4f443ea1
fersilverio <ssilveriofernando45@gmail.com>
2024-03-20 21:26:40 -0300

    ListUsersUseCase unit tests - part 1

---
+++ src/users/application/usecases/__tests__/unit/listusers.usecase.spec.ts
@@ -0,0 +1,54 @@
+import { ListUsersUseCase } from '../../listusers.usecase'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/user-in-memory.repository'
+
+describe('ListUsersUseCase unit tests', () => {
+  let sut: ListUsersUseCase.UseCase
+  let repository: UserInMemoryRepository
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new ListUsersUseCase.UseCase(repository)
+  })
+
+  it('toOutput method', () => {
+    let result = new UserRepository.SearchResult({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    })
+    let output = sut['toOutput'](result)
+    expect(output).toStrictEqual({
+      items: [],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    })
+
+    const entity = new UserEntity(UserDataBuilder({}))
+    result = new UserRepository.SearchResult({
+      items: [entity],
+      total: 1,
+      currentPage: 1,
+      perPage: 2,
+      sort: null,
+      sortDir: null,
+      filter: null,
+    })
+    output = sut['toOutput'](result)
+    expect(output).toStrictEqual({
+      items: [entity.toJSON()],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 2,
+    })
+  })
+})
