d87b1edd54834a9c05e9cb6f4b22904c609f1748
fersilverio <ssilveriofernando45@gmail.com>
2024-03-21 21:09:41 -0300

    UpdateUserUseCase unit tests

---
+++ src/users/application/usecases/__tests__/unit/update-user.usecase.spec.ts
@@ -0,0 +1,44 @@
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { BadRequestError } from '@/shared/application/errors/bad-request-error'
+import { UpdateUserUseCase } from '../../update-user.usecase '
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/user-in-memory.repository'
+
+describe('UpdateUserUseCase unit tests', () => {
+  let sut: UpdateUserUseCase.UseCase
+  let repository: UserInMemoryRepository
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    sut = new UpdateUserUseCase.UseCase(repository)
+  })
+
+  it('Should throw error when entity not found', async () => {
+    await expect(() =>
+      sut.execute({ id: 'fakeId', name: 'test name' }),
+    ).rejects.toThrow(new NotFoundError('Entity not found'))
+  })
+
+  it('Should throw error when name not provided', async () => {
+    await expect(() => sut.execute({ id: 'fakeId', name: '' })).rejects.toThrow(
+      new BadRequestError('Name not provided'),
+    )
+  })
+
+  it('Should update an user', async () => {
+    const spyUpdate = jest.spyOn(repository, 'update')
+    const items = [new UserEntity(UserDataBuilder({}))]
+    repository.items = items
+
+    const result = await sut.execute({ id: items[0]._id, name: 'new name' })
+    expect(spyUpdate).toHaveBeenCalledTimes(1)
+    expect(result).toMatchObject({
+      id: items[0].id,
+      name: 'new name',
+      email: items[0].email,
+      password: items[0].password,
+      createdAt: items[0].createdAt,
+    })
+  })
+})
