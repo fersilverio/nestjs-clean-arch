409c4e8202fd81d22dea8c0ece7a226464c1d85e
fersilverio <ssilveriofernando45@gmail.com>
2024-04-01 20:00:16 -0300

    Add prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { PrismaService } from './shared/infrastructure/database/prisma/prisma.service';
 
 @Module({
-  imports: [EnvConfigModule, UsersModule],
+  imports: [EnvConfigModule, UsersModule, DatabaseModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule { }
