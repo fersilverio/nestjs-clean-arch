8930a02c0921114377f80e447dd6aee382fd3f78
fersilverio <ssilveriofernando45@gmail.com>
2024-04-09 21:04:19 -0300

    Refactoring unit tests - user entity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -16,16 +16,16 @@ describe("UserEntity unit tests", () => {
   it('Constructor method', () => {
     expect(UserEntity.validate).toHaveBeenCalled()
 
-    expect(sut.props.name).toEqual(props.name)
-    expect(sut.props.email).toEqual(props.email)
-    expect(sut.props.password).toEqual(props.password)
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.name).toEqual(props.name)
+    expect(sut.email).toEqual(props.email)
+    expect(sut.password).toEqual(props.password)
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 
   it('Getter of name field', () => {
-    expect(sut.props.name).toBeDefined()
-    expect(sut.props.name).toEqual(props.name)
-    expect(typeof sut.props.name).toBe("string")
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe("string")
   })
 
   it('Setter of name field', () => {
@@ -36,15 +36,15 @@ describe("UserEntity unit tests", () => {
   })
 
   it('Getter of email field', () => {
-    expect(sut.props.email).toBeDefined()
-    expect(sut.props.email).toEqual(props.email)
-    expect(typeof sut.props.email).toBe("string")
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe("string")
   })
 
   it('Getter of password field', () => {
-    expect(sut.props.password).toBeDefined()
-    expect(sut.props.password).toEqual(props.password)
-    expect(typeof sut.props.password).toBe("string")
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe("string")
   })
 
   it('Setter of password field', () => {
@@ -55,8 +55,8 @@ describe("UserEntity unit tests", () => {
   })
 
   it('Getter of createdAt field', () => {
-    expect(sut.props.createdAt).toBeDefined()
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 
 
